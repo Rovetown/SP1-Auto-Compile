7a5d5a436acc27f38120b4561259dfce7edd39ac
Rovetown <110688035+Rovetown@users.noreply.github.com>
2023-05-26 22:56:53 +0200

    Added newer Version containing auto-run
    Added new Script version with minimal change to auto run the compiled file to instantly check if everything works. Also updated $directory to show actual path instead of "." if run in same directory as file.

---
+++ auto_compile&run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Überprüfen, ob der Dateiname als Parameter angegeben wurde
+if [ $# -eq 0 ]; then
+  echo "Bitte geben Sie den Dateinamen als Parameter an."
+  exit 1
+fi
+
+# Überprüfen, ob die angegebene Datei existiert
+if [ ! -f "$1" ]; then
+  echo "Die angegebene Datei existiert nicht."
+  exit 1
+fi
+
+# Extrahieren des Verzeichnisses aus dem Dateinamen
+directory=$(dirname "$1")
+
+# Aktuellen Verzeichnisnamen ermitteln, falls $directory = "."
+if [ "$directory" = "." ]; then
+  directory=$(pwd)
+fi
+
+# Executablename
+executableName=$(basename "$1" .c)
+
+# Kompilieren der angegebenen Datei mit den gcc-Parametern
+gcc -std=c11 -pedantic -Wall -Werror -D_XOPEN_SOURCE=700 "$1" -o "${1%.*}"
+
+# Überprüfen, ob die Kompilierung erfolgreich war (gibt Verzeichnis und Dateiname an)
+if [ $? -eq 0 ]; then
+  echo "Kompilierung erfolgreich! Das ausführbare Programm befindet sich im Verzeichnis: $directory mit dem Namen: $executableName"
+  $directory/$executableName
+else
+  echo "Fehler beim Kompilieren."
+fi
